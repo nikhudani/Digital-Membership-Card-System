4b6921c3a1c90239e3384016adda1d041dceb0f9
nikhudani <nikdini2001@gmail.com>
2026-01-13 21:42:33 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,67 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Roles\Role;
+use App\Models\Roles\RoleGroup;
+use App\Models\User;
+use App\Models\UserDetail;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // =====================
+        // Create Users
+        // =====================
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name'              => fake()->name(),
+            'email'             => 'admin@email.com',
+            'email_verified_at' => now(),
+            'password'          => '123',
+            'is_active'         => true,
+            'remember_token'    => Str::random(10),
+        ]);
+
+        User::create([
+            'name'              => fake()->name(),
+            'email'             => 'user@email.com',
+            'email_verified_at' => now(),
+            'password'          => 'Abcd1234',
+            'is_active'         => true,
+            'remember_token'    => Str::random(10),
+        ]);
+
+        Role::create([
+            'name'          => 'Admin',
+            'description'   => 'All rights to the whole panel.',
+        ]);
+
+        Role::create([
+            'name'          => 'User',
+            'description'   => 'Can access to specific functions.',
+        ]);
+
+        Role::create([
+            'name'          => 'Customer',
+            'description'   => 'Purchasing products.',
+        ]);
+
+        RoleGroup::create([
+            'user_id'   => 1,
+            'role_id'   => 1,
+        ]);
+
+        RoleGroup::create([
+            'user_id'   => 2,
+            'role_id'   => 2,
+        ]);
+
     }
 }
